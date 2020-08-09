28b9e8b263ccc86773113c48bdcf9036aa85e676
AniBiNGi <bingia30@gmail.com>
2020-08-09 12:04:09 -0500

    database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var ball;
+var database;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database()
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var positionnode = database.ref("ball/position")
+    positionnode.on("value", readDB)
 }
 
 function draw(){
@@ -24,6 +28,14 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+  database.ref("ball/position").set({
+      "x" : position.x + x,
+      "y" : position.y + y
+  })
+}
+
+function readDB(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
 }
